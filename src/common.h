b1fff19bb629aac36d2349c050e43de30c3fdb48
i44p <jsbbdjzjsjawk@gmail.com>
2025-02-06 10:56:09 +0300

    change the matrix size stepping algorithm

--- src/common.h
+++ src/common.h
@@ -29,6 +29,7 @@
 // Squared matrices are tested within a certain range (e.g. 1024x1024, 2048x2048, 4096x4096)
 #define MINSIZE (1*1024)
 #define MAXSIZE (8*1024)
+#define STEP    (1*1024)
 
 // Set the alpha and beta values for the cuBLAS and clBlas libraries. Note that the myGEMM kernels
 // for simplicity only support alpha values of 1 and beta values of 0.
