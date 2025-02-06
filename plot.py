33bf92b39e8952dc0317246e5819107fc6fce8c5
i44p <jsbbdjzjsjawk@gmail.com>
2025-02-06 12:00:19 +0300

    update plot

--- plot.py
+++ plot.py
@@ -12,8 +12,43 @@ for col in data.columns:
         data[col] = data[col].str.strip()
 
 
-def plot_matrices(frame, backend='myGEMM.cl', WGS = 16, opts='-cl-std=CL1.2'):
+def plot_matrices(frame, backend='myGEMM.cl', WGS = 16, opts='-cl-std=CL1.2', log=True):
     fig, ax = plt.subplots(layout='constrained', figsize=(9,5))
+
+    frame = frame[frame['backend'] == backend]
+    frame = frame[frame['cl_compiler_options'] == opts]
+
+    ax.set_xlabel("Matrix size")
+    ax.set_ylabel("Time spent (s), median")
+    ax.set_title(f"{backend}, WGS={WGS}, {opts}")
+    if log:
+        ax.set_yscale('log')
+
+    unique_kernels = frame['selected_kernel'].unique().tolist()
+    unique_sizes = frame['matrix_dimensions'].unique().tolist()
+
+    fmt = [f'{marker}--' for marker in ['o', 'v', '^', '8', 's', 'P', '*', 'D']]
+
+    x = sorted(unique_sizes)
+    y = {}
+    for kernel in unique_kernels:
+        kernel_frame = frame[frame['selected_kernel'] == kernel]
+        times = []
+        for size in x:
+            times.append(kernel_frame[kernel_frame['matrix_dimensions'] == size]['elapsed_s'].median())
+        y[kernel] = times
+    
+    ax.set_xticks(x, x)
+    for kernel in unique_kernels:
+        ax.plot(x, y[kernel], fmt[kernel], label=f"kernel={kernel}", markersize=8)
+
+    ax.legend(loc='upper left', ncols=3)
+
+    fig.savefig(f"plot/plot_matrices{'_log' if log else ''}.png", dpi=300)
+
+
+def grouped_bar_kernels(frame, backend='myGEMM.cl', WGS = 16, opts='-cl-std=CL1.2'):
+    fig, ax = plt.subplots(layout='constrained', figsize=(13,5))
 
     frame = frame[frame['backend'] == backend]
     frame = frame[frame['cl_compiler_options'] == opts]
@@ -26,39 +61,6 @@ def plot_matrices(frame, backend='myGEMM.cl', WGS = 16, opts='-cl-std=CL1.2'):
     unique_kernels = frame['selected_kernel'].unique().tolist()
     unique_sizes = frame['matrix_dimensions'].unique().tolist()
 
-    fmt = [f'{marker}--' for marker in ['o', 'v', '^', '8', 's', 'P', '*', 'D']]
-
-    x = sorted(unique_sizes)
-    y = {}
-    for kernel in unique_kernels:
-        kernel_frame = frame[frame['selected_kernel'] == kernel]
-        times = []
-        for size in unique_sizes:
-            times.append(kernel_frame[kernel_frame['matrix_dimensions'] == size]['elapsed_s'].median())
-        y[kernel] = times
-    
-    ax.set_xticks(x, x)
-    for kernel in unique_kernels:
-        ax.plot(x, y[kernel], fmt[kernel], label=f"kernel={kernel}", markersize=8)
-
-    ax.legend(loc='upper left', ncols=3)
-
-    fig.savefig("plot/plot_matrices.png", dpi=300)
-
-
-def grouped_bar_kernels(frame, backend='myGEMM.cl', WGS = 16, opts='-cl-std=CL1.2'):
-    fig, ax = plt.subplots(layout='constrained', figsize=(13,5))
-
-    frame = frame[frame['backend'] == backend]
-    frame = frame[frame['cl_compiler_options'] == opts]
-
-    ax.set_xlabel("Matrix size")
-    ax.set_ylabel("Time spent (s), median")
-    ax.set_title(f"{backend}, WGS={WGS}, {opts}")
-
-    unique_kernels = frame['selected_kernel'].unique().tolist()
-    unique_sizes = frame['matrix_dimensions'].unique().tolist()
-
     groups = sorted(unique_sizes)
 
     kernel_times = {}
@@ -66,7 +68,7 @@ def grouped_bar_kernels(frame, backend='myGEMM.cl', WGS = 16, opts='-cl-std=CL1.
         kernel_frame = frame[frame['selected_kernel'] == kernel]
         times = []
         for size in groups:
-            times.append(kernel_frame[kernel_frame['matrix_dimensions'] == size]['elapsed_s'].round(2).median())
+            times.append(kernel_frame[kernel_frame['matrix_dimensions'] == size]['elapsed_s'].median())
         kernel_times[kernel] = times
 
     x = np.arange(len(groups))  # the label locations
@@ -112,3 +114,4 @@ def boxplot_kernels(frame, backend='myGEMM.cl', WGS = 16, SIZE=2048):
 grouped_bar_kernels(data)
 boxplot_kernels(data)
 plot_matrices(data)
+plot_matrices(data, log=False)
